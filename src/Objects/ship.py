646d02009c7edeb6fa1220cca781c2d2d759d6ea
Andrew Carl <chuckles.theman@gmail.com>
2014-10-11 03:14:24 -0400

    ship get

---
+++ src/Objects/ship.py
@@ -0,0 +1,19 @@
+import os
+import cocos
+from cocos.sprite import *
+from pyglet import image
+
+
+class Ship( ):
+    def __init__(self):
+        # super( Ship, self ).__init()
+
+        pic = image.load(os.path.normpath(r'..\assets/Graphics/BookCraft.png'))
+        self.sprite = Sprite(pic, (250, 250))
+
+    def move(self, dx, dy):
+        move = MoveBy((dx, dy))
+        self.sprite.do(move)
+
+    def draw(self):
+        self.sprite.draw()
--- src/Scene/combatScene.py
+++ src/Scene/combatScene.py
@@ -3,12 +3,20 @@ from Layers import titleText
 import pyglet
 from cocos.audio.pygame.music import *
 import os
+from Objects import ship
 
 
 class CombatScene( cocos.scene.Scene ):
     def __init__(self):
         super( CombatScene, self ).__init__()
-        # self.enable_handlers()
+        self.bookCraft = ship.Ship()
+        self.add(self.bookCraft.sprite, )
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        print dx, dy
+
+    def on_mouse_press(self, *args):
+        print args
 
     def on_enter(self):
         super(CombatScene,self).on_enter()
